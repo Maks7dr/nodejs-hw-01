b890e38ff383cc05f2215b8cd3d77c021e33d8f4
Maksym Drachenko <maks7d@gmail.com>
2025-06-24 18:51:14 +0500

    add start 2 step

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require('node:fs/promises');
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return [];
+    }
+    console.log(error.massage);
+    throw error;
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require('node:fs/promises');
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const dataToWrite = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, dataToWrite, 'utf-8');
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
