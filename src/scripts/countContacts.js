7a4d44115eb87fedcc6b6e3d1a0bc73c7ad0c1b0
Maksym Drachenko <maks7d@gmail.com>
2025-06-26 12:44:52 +0500

    finish hwn1

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,9 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  const contacts = await readContacts();
+  const arrLength = contacts.length;
+  return arrLength;
+};
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,9 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await readContacts();
+  const arrContacts = contacts.map((item) => item.name);
+  return arrContacts;
+};
 
 console.log(await getAllContacts());
